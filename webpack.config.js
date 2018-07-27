0294cb90f4c3c7329b9f9b0770e603413365deae
Rene Enriquez <renriquez@Renes-Mac-mini.local>
2018-07-27 11:12:05 -0500

    #19 Splitting bpm-people into ui and api

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[name]_[local]_[hash:base64]',
+              sourceMap: true,
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [htmlWebpackPlugin],
+};
