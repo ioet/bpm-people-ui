4ed80d67ff3604bba6940e68f350a50b16dea432
Lukas Ziefle <lziefle@ioet.com>
2018-09-18 10:18:31 -0500

    removed some errors

--- src/component/container/ListData.js
+++ src/component/container/ListData.js
@@ -1,45 +1,43 @@
-import {connect} from 'react-redux'
-import List from "../presentational/List";
+import { connect } from 'react-redux';
+import List from '../presentational/List';
 
 function handleErrors(response) {
-    if (!response.ok) {
-        alert(response.statusText);
-        throw Error(response.statusText);
-    }
-    return response;
+  if (!response.ok) {
+    alert(response.statusText);
+    throw Error(response.statusText);
+  }
+  return response;
 }
 
 const getUsers = (dispatch) => {
-    let url = 'http://localhost:3001/people';
-    fetch(url)
-        .then(handleErrors)
-        .then(res => res.json())
-        .then(result => {
-            dispatch({
-                type: 'ADD_USERS',
-                user: result
-            });
-            return result;
-        })
-        .catch(function (error) {
-            alert(error);
-            console.log(error);
-        });
+  const url = 'http://localhost:3001/people';
+  fetch(url)
+    .then(handleErrors)
+    .then(res => res.json())
+    .then((result) => {
+      dispatch({
+        type: 'ADD_USERS',
+        user: result,
+      });
+      return result;
+    })
+    .catch((error) => {
+      alert(error);
+      console.log(error);
+    });
 };
 
-const mapStateToProps = (state) => {
-    return {
-        userList: state.userList
-    }
-};
+const mapStateToProps = state => ({
+  userList: state.userList,
+});
 
 const mapDispatchToProps = (dispatch) => {
-    getUsers(dispatch);
+  getUsers(dispatch);
 };
 
 const ListData = connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps,
 )(List);
 
 export default ListData;
