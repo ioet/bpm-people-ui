6d96aee5f6b4f6263ab0a6ca362adbb9f8ed27b6
Lukas Ziefle <lziefle@ioet.com>
2018-09-20 11:01:36 -0500

    update user per api

--- src/component/utils/Utils.js
+++ src/component/utils/Utils.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len,no-undef */
 
-export const PEOPLE_API = 'http://localhost:3001/people';
+export const PEOPLE_API = 'http://localhost:3000/people';
 
 export function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
