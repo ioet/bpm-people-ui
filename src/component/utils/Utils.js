fa7734cf126a4de1a7d8b93e443177bbc49f6591
Lukas Ziefle <lziefle@ioet.com>
2018-10-02 15:14:41 -0500

    configured axios

--- src/component/utils/Utils.js
+++ src/component/utils/Utils.js
@@ -1,8 +1,5 @@
 /* eslint-disable max-len,no-undef */
 
-// export const PEOPLE_API = 'http://localhost:3000/people';
-export const PEOPLE_API = process.env.REACT_APP_BPM_PEOPLE_API_URL;
-
 export function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
